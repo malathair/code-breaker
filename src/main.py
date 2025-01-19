ad5b503c4e402ef86bf8aedf14f273f3c7076421
malathair <malathair@spectregate.net>
2025-01-18 17:49:36 -0700

    Added coloring and fixed a bug relating to difficulty level and the secret code length

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
+import enum
 import random
 
+
 MAX_GUESSES = 10
+
+
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+DEFAULT = "\033[38m"
+RESET = "\033[39m"
 
 
 def clear() -> None:
@@ -25,7 +39,7 @@ class CodeBreaker:
     def __check_guess(self, guess: str) -> None:
         if guess == self.__secret_code:
             self.__player_has_won = True
-            return "ccc"
+            return GREEN + "ccc" + RESET
 
         clues = {}
         for index, num in enumerate(guess):
@@ -36,12 +50,12 @@ class CodeBreaker:
                 clues[num] = ""
 
             if num == self.__secret_code[index]:
-                clues[num] = "c"
+                clues[num] = GREEN + "c"
             else:
                 if not clues[num]:
-                    clues[num] = "w"
+                    clues[num] = YELLOW + "w"
 
-        return "".join(sorted([clue for _, clue in clues.items() if clue]))
+        return "".join(sorted([clue for _, clue in clues.items() if clue])) + RESET
 
     def __display_game_end(self) -> None:
         if self.__player_has_won:
@@ -54,12 +68,13 @@ class CodeBreaker:
     # Generate a random code. The length of which is determined by the game difficulty
     def __gen_code(self) -> None:
         length = int(self.__difficulty) + 2
-        number = str(random.randint(0, 999))
+        number = str(random.randint(0, 10**length - 1))
         self.__secret_code = number.zfill(length - len(number))
+        input(", ".join([str(self.__difficulty), str(length), number, self.__secret_code]))
 
     def __make_guess(self) -> None:
         if self.__last_guess_invalid:
-            print("You entered an invalid or duplicate guess. Please try again:")
+            print(RED + "You entered an invalid or duplicate guess!" + RESET + "Please try again:")
         else:
             print("Please make a guess:")
         guess = input("> ")
@@ -85,7 +100,7 @@ class CodeBreaker:
         finally:
             pass
 
-    def __print_board(self) -> None:
+    def __draw_board(self) -> None:
         print("A secret code has been set! Can you break it?", "\n")
 
         if len(self.__history) > 0:
@@ -106,19 +121,19 @@ class CodeBreaker:
         while not difficulty_set:
             clear()
             print("Please select a difficulty:", "\n")
-            print("  1. Easy   (3 digit code)")
-            print("  2. Medium (4 digit code)")
-            print("  3. Hard   (5 digit code)", "\n")
+            print("  1. " + GREEN + "Easy" + RESET + "   (3 digit code)")
+            print("  2. " + YELLOW + "Medium" + RESET + " (4 digit code)")
+            print("  3. " + RED + "Hard" + RESET + "   (5 digit code)", "\n")
 
             if invalid_choice:
-                print("Invalid selection! Please select a valid difficulty:")
+                print(RED + "Invalid selection! Please select a valid difficulty:" + RESET)
                 invalid_choice = False
 
             choice = input("> ")
 
             try:
-                self.__difficulty = int(choice) + 2
-                if self.__difficulty < 3 or self.__difficulty > 5:
+                self.__difficulty = int(choice)
+                if self.__difficulty < 1 or self.__difficulty > 3:
                     raise ValueError("Code length must be between 3 and 5")
             except ValueError:
                 invalid_choice = True
@@ -130,7 +145,7 @@ class CodeBreaker:
     def run(self) -> None:
         while len(self.__history) < MAX_GUESSES and not self.__player_has_won:
             clear()
-            self.__print_board()
+            self.__draw_board()
             self.__make_guess()
 
         self.__display_game_end()
